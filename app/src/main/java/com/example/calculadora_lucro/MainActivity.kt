fc600b2fca89054897b130a8ece03b8ceb1d19ef
Livia Nakashima <43746512+LiviaNakashima@users.noreply.github.com>
2020-09-07 01:56:43 -0300

    Adicionando as validações do botão

--- app/src/main/java/com/example/calculadora_lucro/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_lucro/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.calculadora_lucro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity() {
 
     fun clicar(componente:View){
 
+     if (et_produto.length() < 2 ){
+         Toast.makeText(this, "O nome do produto deve ter mais que 1 letra", Toast.LENGTH_SHORT).show()
+     } else {
+         val produto = et_produto.text }
+
+     if (et_receita.length() > 200){
+         Toast.makeText(this, "A receita não pode ser maior que 200", Toast.LENGTH_SHORT).show()
+     } else{
+         val receita = et_receita.text
+     }
     }
 }
