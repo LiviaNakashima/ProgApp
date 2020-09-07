4a274a00fcfa0c0f62d3455652ff37a47add57bf
Livia Nakashima <43746512+LiviaNakashima@users.noreply.github.com>
2020-09-07 00:28:42 -0300

    Função de clicar

--- app/src/main/java/com/example/calculadora_lucro/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_lucro/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculadora_lucro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clicar(componente:View){
+
     }
 }
