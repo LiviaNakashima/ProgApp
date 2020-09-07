eba348cb3a9b926ee20daf999af32eb46ea84724
Livia Nakashima <43746512+LiviaNakashima@users.noreply.github.com>
2020-09-07 17:48:13 -0300

    Validações e resultados dos cálculos

--- app/src/main/java/com/example/calculadora_lucro/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_lucro/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadora_lucro
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,17 +13,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun clicar(componente:View){
+    fun calcular(componente:View){
+        if (et_produto.length() < 2 ){
+            Toast.makeText(this, "Valor incorreto, o nome do produto deve ter pelo menos 2 letras.", Toast.LENGTH_SHORT).show()
+        } else if (et_receita.getText().toString().trim().equals("") || et_custo.getText().toString().trim().equals("")){
+            Toast.makeText(this, "Valores incorretos, todos os campos devem estar preenchidos.",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            val lucro = et_receita.text.toString().toDouble() - et_custo.text.toString().toDouble()
+            val margemLucro = (lucro/et_receita.text.toString().toDouble())*100
 
-     if (et_produto.length() < 2 ){
-         Toast.makeText(this, "O nome do produto deve ter mais que 1 letra", Toast.LENGTH_SHORT).show()
-     } else {
-         val produto = et_produto.text }
-
-     if (et_receita.length() > 200){
-         Toast.makeText(this, "A receita não pode ser maior que 200", Toast.LENGTH_SHORT).show()
-     } else{
-         val receita = et_receita.text
-     }
+            if(margemLucro < 35.0){
+                tv_frase.visibility = View.VISIBLE
+                tv_frase.setTextColor(Color.RED)
+                tv_frase.text = "Vixi, você tem R$${lucro.toString()} de lucro com seu produto ${et_produto.text}, isso significa que sua margem de lucro" +
+                        " é de ${margemLucro}%.Você precisa estudar mais para não sair no prejuízo"
+            } else if (margemLucro > 50.0){
+                tv_frase.visibility = View.VISIBLE
+                tv_frase.setTextColor(Color.GREEN)
+                tv_frase.text = "MEU DEUS! você tem R$${lucro} de lucro com seu produto ${et_produto.text}, isso significa que sua margem de lucro" +
+                        " é de ${margemLucro}%.Você é o deus dos lucros!!!"
+            } else{
+                tv_frase.setTextColor(Color.BLUE)
+                tv_frase.text = "Caramba, você tem R$${lucro} de lucro com seu produto ${et_produto.text}, isso significa que sua margem de lucro" +
+                        " é de ${margemLucro}%.Você está se saindo bem!"
+                tv_frase.visibility = View.VISIBLE
+            }
+        }
     }
+
 }
